5f8b3a859170a648ec84eefd25d9f89632772885
yaohsin <iisaintz@gmail.com>
2021-05-21 15:10:24 +0800

    update

--- playground.js
+++ playground.js
@@ -1,8 +1,15 @@
 const axios = require('axios');
 
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  })
+}
+
 const test = async () => {
   try {
     const result = await axios.get(`https://polkadot.w3f.community/valid`);
+    await sleep(600000);
   } catch (err) {
     console.log(`err`);
     console.log(err);
